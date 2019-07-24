977dfc5b243b710ae85fc6c9ec6dc254079843de
caioguilherme10 <caioguilhermeuna@gmail.com>
2019-07-24 12:03:28 -0300

    construção basica das classes beneficio,aluno e justificativaCaest

--- src/main/java/br/com/loopis/controle_refeicoes/modelo/entidades/Aluno.java
+++ src/main/java/br/com/loopis/controle_refeicoes/modelo/entidades/Aluno.java
@@ -1,11 +1,57 @@
 package br.com.loopis.controle_refeicoes.modelo.entidades;
 
+import java.io.Serializable;
 import javax.persistence.Id;
 
-public class Aluno {
+public class Aluno implements Serializable{
 
     @Id
     private int id;
     private String matricula;
     private String nome;
+
+    public Aluno() {
+    }
+
+    public Aluno(int id, String matricula, String nome) {
+        this.id = id;
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public Aluno(String matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "id=" + id + ", matricula=" + matricula + ", nome=" + nome + '}';
+    }
+    
+    
 }
--- src/main/java/br/com/loopis/controle_refeicoes/modelo/entidades/JustificativaCAEST.java
+++ src/main/java/br/com/loopis/controle_refeicoes/modelo/entidades/JustificativaCAEST.java
@@ -1,14 +1,68 @@
 package br.com.loopis.controle_refeicoes.modelo.entidades;
 
+import java.io.Serializable;
 import javax.persistence.Id;
 
-public class JustificativaCAEST {
+public class JustificativaCAEST implements Serializable{
 
     @Id
     private int id;
     private Pedido pedido;
     private String justificativa;
     private Usuario usuarioCAEST;
+
+    public JustificativaCAEST() {
+    }
+
+    public JustificativaCAEST(int id, Pedido pedido, String justificativa, Usuario usuarioCAEST) {
+        this.id = id;
+        this.pedido = pedido;
+        this.justificativa = justificativa;
+        this.usuarioCAEST = usuarioCAEST;
+    }
+
+    public JustificativaCAEST(Pedido pedido, String justificativa, Usuario usuarioCAEST) {
+        this.pedido = pedido;
+        this.justificativa = justificativa;
+        this.usuarioCAEST = usuarioCAEST;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public String getJustificativa() {
+        return justificativa;
+    }
+
+    public void setJustificativa(String justificativa) {
+        this.justificativa = justificativa;
+    }
+
+    public Usuario getUsuarioCAEST() {
+        return usuarioCAEST;
+    }
+
+    public void setUsuarioCAEST(Usuario usuarioCAEST) {
+        this.usuarioCAEST = usuarioCAEST;
+    }
+
+    @Override
+    public String toString() {
+        return "JustificativaCAEST{" + "id=" + id + ", pedido=" + pedido + ", justificativa=" + justificativa + ", usuarioCAEST=" + usuarioCAEST + '}';
+    }
 
 
 }
