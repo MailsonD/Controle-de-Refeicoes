d9dfbf4b541000053132f5f66589817533f2249e
Leanderson Coelho <leanderson.coelhoif@gmail.com>
2019-07-24 15:41:43 -0300

    Pedido Dao Impl

--- src/main/java/br/com/loopis/controle_refeicoes/modelo/dao/implementacoes/PedidoDaoImpl.java
+++ src/main/java/br/com/loopis/controle_refeicoes/modelo/dao/implementacoes/PedidoDaoImpl.java
@@ -3,31 +3,39 @@ package br.com.loopis.controle_refeicoes.modelo.dao.implementacoes;
 import br.com.loopis.controle_refeicoes.modelo.dao.interfaces.PedidoDao;
 import br.com.loopis.controle_refeicoes.modelo.entidades.Pedido;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Stateless
 public class PedidoDaoImpl implements PedidoDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public void salvar(Pedido object) {
-
+        em.persist(object);
     }
 
     @Override
     public void atualizar(Pedido object) {
-
+        em.merge(object);
     }
 
     @Override
     public void remover(Pedido object) {
-
+        em.remove(em.merge(object));
     }
 
     @Override
-    public Pedido buscar(Object key) {
-        return null;
+    public Pedido buscar(Object key){
+        return em.find(Pedido.class,key);
     }
 
     @Override
-    public List<Pedido> listar() {
-        return null;
+    public List<Pedido> listar(){
+        return em.createQuery("SELECT p FROM Pedido p").getResultList();
     }
 }
