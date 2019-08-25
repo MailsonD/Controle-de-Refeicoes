94f6db1dcd27e04a97bb10a009d0a7b632901366
MailsonD <mailssondennis@gmail.com>
2019-08-25 16:32:49 -0300

    Corrigindo configurações do banco de dados

--- src/main/java/br/com/loopis/controle_refeicoes/controladores/testes/TestBean.java
+++ src/main/java/br/com/loopis/controle_refeicoes/controladores/testes/TestBean.java
@@ -11,6 +11,8 @@ import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Singleton
 @Startup
@@ -25,18 +27,18 @@ public class TestBean {
 
     @PostConstruct
     private void init() {
-//        Usuario u1 = new Usuario("1", "1", "1", "Usuario1", NivelAcesso.PROFESSOR, Boolean.TRUE);
-//        Usuario u2 = new Usuario("2", "2", "2", "Usuario2", NivelAcesso.ADMINISTRADOR, Boolean.TRUE);
-//        Usuario u3 = new Usuario("3", "3", "3", "Usuario3", NivelAcesso.CAEST, Boolean.TRUE);
-//        Usuario u4 = new Usuario("4", "4", "4", "Usuario4", NivelAcesso.GESTOR, Boolean.TRUE);
-//        try {
-//            usuarioDao.salvar(u1);
-//            usuarioDao.salvar(u2);
-//            usuarioDao.salvar(u3);
-//            usuarioDao.salvar(u4);
-//        } catch (MatriculaExistenteException ex) {
-//            Logger.getLogger(TestBean.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        Usuario u1 = new Usuario("1", "1", "1", "Usuario1", NivelAcesso.PROFESSOR, Boolean.TRUE);
+        Usuario u2 = new Usuario("2", "2", "2", "Usuario2", NivelAcesso.ADMINISTRADOR, Boolean.TRUE);
+        Usuario u3 = new Usuario("3", "3", "3", "Usuario3", NivelAcesso.CAEST, Boolean.TRUE);
+        Usuario u4 = new Usuario("4", "4", "4", "Usuario4", NivelAcesso.GESTOR, Boolean.TRUE);
+        try {
+            usuarioDao.salvar(u1);
+            usuarioDao.salvar(u2);
+            usuarioDao.salvar(u3);
+            usuarioDao.salvar(u4);
+        } catch (MatriculaExistenteException ex) {
+            Logger.getLogger(TestBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
 //        try {
     //        List<Usuario> l = new ArrayList<>();
     //        l.add(new Usuario("123", "321", "email@gmail.com", "Zé", NivelAcesso.ADMINISTRADOR));
@@ -65,17 +67,17 @@ public class TestBean {
 //            Logger.getLogger(TestBean.class.getName()).log(Level.SEVERE, null, ex);
 //        }
 
-        Usuario um = new Usuario("123","0123","caio@gmail.com","caio",NivelAcesso.ADMINISTRADOR);
-        try {
-            serviceUsuario.salvar(um);
-        } catch (MatriculaExistenteException ex) {
-            System.out.println(ex.getMessage());
-        }
-        Usuario dois = new Usuario("123","0123","caio@gmail.com","caio",NivelAcesso.ADMINISTRADOR);
-        try {
-            serviceUsuario.salvar(dois);
-        } catch (MatriculaExistenteException ex) {
-            System.out.println(ex.getMessage());
-        }
+//        Usuario um = new Usuario("123","0123","caio@gmail.com","caio",NivelAcesso.ADMINISTRADOR);
+//        try {
+//            serviceUsuario.salvar(um);
+//        } catch (MatriculaExistenteException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        Usuario dois = new Usuario("123","0123","caio@gmail.com","caio",NivelAcesso.ADMINISTRADOR);
+//        try {
+//            serviceUsuario.salvar(dois);
+//        } catch (MatriculaExistenteException ex) {
+//            System.out.println(ex.getMessage());
+//        }
     }
 }
