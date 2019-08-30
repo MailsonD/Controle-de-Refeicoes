e927af3f3bdbe100b598b22bbc5130b5198ecd20
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-08-30 14:42:26 -0300

    Consulta de pedidos enviando a chave de professor Resultado com paginção

--- src/main/java/br/com/loopis/controle_refeicoes/modelo/entidades/Pedido.java
+++ src/main/java/br/com/loopis/controle_refeicoes/modelo/entidades/Pedido.java
@@ -1,12 +1,14 @@
 package br.com.loopis.controle_refeicoes.modelo.entidades;
 
 import br.com.loopis.controle_refeicoes.modelo.conversor.DataConversor;
+import br.com.loopis.controle_refeicoes.modelo.entidades.enums.StatusPedido;
 import br.com.loopis.controle_refeicoes.modelo.entidades.enums.TipoBeneficio;
 import br.com.loopis.controle_refeicoes.modelo.entidades.enums.Turma;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,39 +29,46 @@ public class Pedido implements Serializable {
     private LocalDate diaSolicitado;
     @Enumerated(EnumType.STRING)
     private Turma turma;
+
+    @Enumerated(EnumType.ORDINAL)
+    private StatusPedido statusPedido;
+
     @Enumerated(EnumType.STRING)
     private TipoBeneficio tipoBeneficio;
     @ManyToMany(cascade = CascadeType.PERSIST)
     private List<Aluno> alunos;
 
-    public Pedido(Long id, Usuario professor, String justificativa, LocalDate diaSolicitado, Turma turma, TipoBeneficio tipoBeneficio, List<Aluno> alunos) {
+    public Pedido(){};
+
+    public Pedido(Long id, Usuario professor, String justificativa, LocalDate diaSolicitado, Turma turma, StatusPedido statusPedido, TipoBeneficio tipoBeneficio, List<Aluno> alunos) {
         this.id = id;
         this.professor = professor;
         this.justificativa = justificativa;
         this.diaSolicitado = diaSolicitado;
         this.turma = turma;
+        this.statusPedido = statusPedido;
         this.tipoBeneficio = tipoBeneficio;
         this.alunos = alunos;
     }
 
-    public Pedido(Usuario professor, String justificativa, LocalDate diaSolicitado, Turma turma, TipoBeneficio tipoBeneficio, List<Aluno> alunos) {
+    public Pedido(Usuario professor, String justificativa, LocalDate diaSolicitado, Turma turma, StatusPedido statusPedido, TipoBeneficio tipoBeneficio, List<Aluno> alunos) {
         this.professor = professor;
         this.justificativa = justificativa;
         this.diaSolicitado = diaSolicitado;
         this.turma = turma;
+        this.statusPedido = statusPedido;
         this.tipoBeneficio = tipoBeneficio;
         this.alunos = alunos;
     }
 
-    public Pedido(Usuario professor, String justificativa, LocalDate diaSolicitado, Turma turma, TipoBeneficio tipoBeneficio) {
+    public Pedido(Usuario professor, String justificativa, LocalDate diaSolicitado, Turma turma, StatusPedido statusPedido, TipoBeneficio tipoBeneficio) {
         this.professor = professor;
         this.justificativa = justificativa;
         this.diaSolicitado = diaSolicitado;
         this.turma = turma;
+        this.statusPedido = statusPedido;
         this.tipoBeneficio = tipoBeneficio;
-    }
-
-    public Pedido() {
+        this.alunos = new ArrayList<>();
     }
 
     public Long getId() {
@@ -102,6 +111,14 @@ public class Pedido implements Serializable {
         this.turma = turma;
     }
 
+    public String getStatusPedido() {
+        return String.valueOf(statusPedido);
+    }
+
+    public void setStatusPedido(StatusPedido statusPedido) {
+        this.statusPedido = statusPedido;
+    }
+
     public TipoBeneficio getTipoBeneficio() {
         return tipoBeneficio;
     }
@@ -119,58 +136,22 @@ public class Pedido implements Serializable {
     }
 
     @Override
-    public String toString() {
-        return "Pedido{" + "id=" + id + ", professor=" + professor + ", justificativa=" + justificativa + ", diaSolicitado=" + diaSolicitado + ", turma=" + turma + ", tipoBeneficio=" + tipoBeneficio + ", alunos=" + alunos + '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pedido pedido = (Pedido) o;
+        return id.equals(pedido.id) &&
+                professor.equals(pedido.professor) &&
+                justificativa.equals(pedido.justificativa) &&
+                diaSolicitado.equals(pedido.diaSolicitado) &&
+                turma == pedido.turma &&
+                statusPedido == pedido.statusPedido &&
+                tipoBeneficio == pedido.tipoBeneficio &&
+                alunos.equals(pedido.alunos);
     }
 
     @Override
     public int hashCode() {
-        int hash = 3;
-        hash = 59 * hash + Objects.hashCode(this.id);
-        hash = 59 * hash + Objects.hashCode(this.professor);
-        hash = 59 * hash + Objects.hashCode(this.justificativa);
-        hash = 59 * hash + Objects.hashCode(this.diaSolicitado);
-        hash = 59 * hash + Objects.hashCode(this.turma);
-        hash = 59 * hash + Objects.hashCode(this.tipoBeneficio);
-        hash = 59 * hash + Objects.hashCode(this.alunos);
-        return hash;
+        return Objects.hash(id, professor, justificativa, diaSolicitado, turma, statusPedido, tipoBeneficio, alunos);
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Pedido other = (Pedido) obj;
-        if (!Objects.equals(this.justificativa, other.justificativa)) {
-            return false;
-        }
-        if (!Objects.equals(this.id, other.id)) {
-            return false;
-        }
-        if (!Objects.equals(this.professor, other.professor)) {
-            return false;
-        }
-        if (!Objects.equals(this.diaSolicitado, other.diaSolicitado)) {
-            return false;
-        }
-        if (this.turma != other.turma) {
-            return false;
-        }
-        if (this.tipoBeneficio != other.tipoBeneficio) {
-            return false;
-        }
-        if (!Objects.equals(this.alunos, other.alunos)) {
-            return false;
-        }
-        return true;
-    }
-    
-    
 }
